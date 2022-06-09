50b0eedfe5ffbd7b33bd1eaf0cbb8b93c3ed79aa
KARTHIKEYAN816 <karthikshan706@gmail.com>
2022-06-09 14:45:32 +0530

    new change

--- src/test/java/com/testrunner/TestRunner.java
+++ src/test/java/com/testrunner/TestRunner.java
@@ -12,7 +12,7 @@ import io.cucumber.junit.CucumberOptions;
 @CucumberOptions(features="src\\test\\resources\\Feature\\Flight.feature",glue="com.stepdefinitions",
 dryRun=false,plugin= {"junit:src\\test\\resources\\Reports\\sample.xml","json:src\\test\\resources\\Reports\\Flight.json"})
 
-public class Flight {
+public class TestRunner {
 @AfterClass	
 public static void name() {
 	JvmReport.Report(System.getProperty("user.dir")+
